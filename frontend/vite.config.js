1d1c9db1f25215b58ea32be8a4869f14541d426b
Kevin Thulnith <112649346+kevinThulnith@users.noreply.github.com>
2025-09-08 21:48:54 +0530

    5th comit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Allow connections from all network interfaces
+    port: 5173,
+  },
 });
